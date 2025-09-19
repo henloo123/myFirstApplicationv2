d5a7a5d7514fc40c8a4dafc42310a4999d673d2b
henloo123 <shua0257@gmail.com>
2025-09-20 02:15:00 +0800

    Complete dynamic layout project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        About
+    </x-slot:heading>
+    This is the content for the About.
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Contact
+    </x-slot:heading>
+    This is the content for the Contact.
+</x-layout>
