87b3e8a5db1ec8aa83547fe3882a832775ecce33
henloo123 <shua0257@gmail.com>
2025-09-20 02:22:18 +0800

    Chapter 2: Implement dynamic job listings with Models

--- resources/views/about.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        About
-    </x-slot:heading>
-    This is the content for the About.
-</x-layout>
--- resources/views/contact.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Contact
-    </x-slot:heading>
-    This is the content for the Contact.
-</x-layout>
