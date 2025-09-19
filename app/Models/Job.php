9cf9e3dc1c2ad0bdc0e773abf4e32b38c30a00b9
henloo123 <shua0257@gmail.com>
2025-09-20 02:45:36 +0800

    Chapter 4: Implement Eloquent relationships

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,7 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
-    // By convention, Laravel assumes a 'jobs' table.
-    // We need to tell it to use our 'job_listings' table instead.
+
     protected $table = 'job_listings';
+
+    public function employer()
+    {
+        return $this->belongsTo(\App\Models\Employer::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(\App\Models\Tag::class, 'job_listing_tag', foreignPivotKey: 'job_listing_id');
+    }
 }
--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -2,6 +2,9 @@
     <x-slot:heading>
         Job
     </x-slot:heading>
+    <p class="text-sm text-gray-500">{{ $job->employer->name }}</p>
     <h2 class="font-bold text-lg">{{ $job['title'] }}</h2>
-    This job pays {{ $job['salary'] }} per year.
+    <p>
+        This job pays {{ $job['salary'] }} per year.
+    </p>
 </x-layout>
